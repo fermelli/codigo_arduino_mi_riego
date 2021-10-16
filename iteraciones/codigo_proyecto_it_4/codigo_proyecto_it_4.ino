aead70f3d178c21164cd1af9a0a68b16f5d0dac3
fermelli <luisfernandosalgadomiguez@gmail.com>
2021-10-16 04:00:02 -0400

    codigo cuarta iteracion

---
+++ iteraciones/codigo_proyecto_it_4/codigo_proyecto_it_4.ino
@@ -0,0 +1,233 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "RTClib.h"
+#include <SPI.h>
+#include <SD.h>
+#include <DHT.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+RTC_DS3231 reloj;
+File miArchivo;
+byte pinCS = 53;
+String nombreArchivo = "data.csv";
+#define pinSensorHRT 2
+#define pinSensorHRTPower 5
+#define tipoSensorHRT DHT11
+DHT sensorHRT(pinSensorHRT, tipoSensorHRT);
+const float pinSHS1 = A0;
+const float pinSHS2 = A1;
+const byte pinReleIN1 = 40;
+const byte pinReleIN2 = 41;
+const byte pinReleIN3 = 42;
+const byte pinLed = 7;
+const byte pinInterruptor = 38;
+bool encendidoBombaAguaRecirculacion = false;
+const int mumeroLecturasParaObtenerPromedio = 10;
+const int tiempoEntreLecturasDelPromedio = 500;
+const long tiempoEntreLecturasRealizadas = 5000;
+const long tiempoEntreGuardadoDeLecturasRegistradas = 10000;
+float promedioPorcentajesSHS1 = 0;
+float promedioPorcentajesSHS2 = 0;
+const int porcentajeMinimoSHS1 = 0;
+const int porcentajeMaximoSHS1 = 70;
+const int porcentajeMinimoSHS2 = 0;
+const int porcentajeMaximoSHS2 = 70;
+const int tiempoRiegoSHS1 = 2000;
+const int tiempoRiegoSHS2 = 2000;
+long tiempoMilisegundos = 0;
+#define pinSensorNivelInferior 3
+#define pinSensorNivelSuperior 4
+boolean aNivelMaximo;
+boolean aNivelMinimo;
+
+void setup()
+{
+    Serial.begin(9600);
+    lcd.init();
+    lcd.backlight();
+    pinMode(pinReleIN1, OUTPUT);
+    pinMode(pinReleIN2, OUTPUT);
+    pinMode(pinReleIN3, OUTPUT);
+    pinMode(pinInterruptor, INPUT_PULLUP);
+    pinMode(pinLed, OUTPUT);
+    digitalWrite(pinReleIN1, HIGH);
+    digitalWrite(pinReleIN2, HIGH);
+    digitalWrite(pinReleIN3, HIGH);
+    if (!reloj.begin())
+    {
+        Serial.println("No se pudo encontrar el Reloj RTC");
+        Serial.flush();
+        abort();
+    }
+    if (reloj.lostPower())
+    {
+        Serial.println("¡El Reloj RTC perdió energía, ¡establezcamos la hora!");
+        reloj.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        /
+    }
+    Serial.println("Inicializando tarjeta SD...");
+    if (!SD.begin(pinCS))
+    {
+        Serial.println("¡Inicializacion fallida!");
+    }
+    else
+    {
+        Serial.println("Inicializacion completa");
+    }
+    sensorHRT.begin();
+    pinMode(pinSensorHRTPower, OUTPUT);
+    digitalWrite(pinSensorHRTPower, HIGH);
+    pinMode(pinSensorNivelInferior, INPUT);
+    pinMode(pinSensorNivelSuperior, INPUT);
+    aNivelMinimo = false;
+    aNivelMaximo = false;
+}
+
+void loop()
+{
+    lecturasSensores(mumeroLecturasParaObtenerPromedio, valorLecturaTierraSeca);
+
+    if (promedioPorcentajesSHS1 >= porcentajeMinimoSHS1 && promedioPorcentajesSHS1 <= porcentajeMaximoSHS1)
+    {
+        digitalWrite(pinReleIN2, LOW);
+        lcd.setCursor(2, 0);
+        lcd.print("BOMBA AGUA 1");
+        lcd.setCursor(2, 1);
+        lcd.print("ACTIVANDO " + String(tiempoRiegoSHS1 / 1000) + "s");
+        delay(tiempoRiegoSHS1);
+        digitalWrite(pinReleIN2, HIGH);
+    }
+
+    if (promedioPorcentajesSHS2 >= porcentajeMinimoSHS2 && promedioPorcentajesSHS2 <= porcentajeMaximoSHS2)
+    {
+        digitalWrite(pinReleIN3, LOW);
+        lcd.setCursor(2, 0);
+        lcd.print("BOMBA AGUA 2");
+        lcd.setCursor(2, 1);
+        lcd.print("ACTIVANDO " + String(tiempoRiegoSHS2 / 1000) + "s");
+        delay(tiempoRiegoSHS2);
+        digitalWrite(pinReleIN3, HIGH);
+    }
+
+    if (digitalRead(pinSensorNivelInferior) == HIGH)
+    {
+        aNivelMinimo = true;
+    }
+    else
+    {
+        aNivelMinimo = false;
+    }
+
+    if (digitalRead(pinSensorNivelSuperior) == HIGH)
+    {
+        aNivelMaximo = true;
+    }
+    else
+    {
+        aNivelMaximo = false;
+    }
+
+    if (aNivelMaximo && aNivelMinimo)
+    {
+        encendidoBombaAguaRecirculacion = true;
+    }
+    else
+    {
+        if (!aNivelMinimo)
+        {
+            encendidoBombaAguaRecirculacion = false;
+        }
+    }
+
+    byte lecturaInterruptor = digitalRead(pinInterruptor);
+    if (encendidoBombaAguaRecirculacion || lecturaInterruptor == HIGH)
+    {
+        digitalWrite(pinReleIN1, LOW);
+        digitalWrite(pinLed, HIGH);
+    }
+    else
+    {
+        digitalWrite(pinReleIN1, HIGH);
+        digitalWrite(pinLed, LOW);
+    }
+}
+
+void lecturasSensores(int numeroLecturas)
+{
+    lcd.clear();
+    lcd.setCursor(4, 0);
+    lcd.print("LEYENDO");
+    lcd.setCursor(2, 1);
+    lcd.print("SENSORES...");
+    float sumaPorcentajesSHS1 = 0;
+    float sumaPorcentajesSHS2 = 0;
+    for (int i = 1; i <= numeroLecturas; i++)
+    {
+        float lecturaSHS1 = analogRead(pinSHS1);
+        float lecturaSHS2 = analogRead(pinSHS2);
+        float porcentajeSHS1 = map(lecturaSHS1, 1021, 305, 0, 100);
+        float porcentajeSHS2 = map(lecturaSHS2, 1022, 275, 0, 100);
+        sumaPorcentajesSHS1 = sumaPorcentajesSHS1 + porcentajeSHS1;
+        sumaPorcentajesSHS2 = sumaPorcentajesSHS2 + porcentajeSHS2;
+        delay(tiempoEntreLecturasDelPromedio);
+    }
+    promedioPorcentajesSHS1 = sumaPorcentajesSHS1 / numeroLecturas;
+    promedioPorcentajesSHS2 = sumaPorcentajesSHS2 / numeroLecturas;
+    DateTime ahora = reloj.now();
+    String marcaDeTiempo = ahora.timestamp();
+    float humedadRelativa = sensorHRT.readHumidity();
+    float temperaturaCelcius = sensorHRT.readTemperature();
+    boolean lecturasSensorHRTSonValidas = !isnan(humedadRelativa) && !isnan(temperaturaCelcius);
+    if (!lecturasSensorHRTSonValidas)
+    {
+        digitalWrite(pinSensorHRTPower, LOW);
+        delay(100);
+        digitalWrite(pinSensorHRTPower, HIGH);
+    }
+    if (millis() - tiempoMilisegundos >= tiempoEntreGuardadoDeLecturasRegistradas || tiempoMilisegundos == 0)
+    {
+        tiempoMilisegundos = millis();
+        miArchivo = SD.open(nombreArchivo, FILE_WRITE);
+        if (miArchivo)
+        {
+            if (lecturasSensorHRTSonValidas)
+            {
+                String registro = String(promedioPorcentajesSHS1) + "," + String(promedioPorcentajesSHS2) + "," + String(humedadRelativa) + "," + String(temperaturaCelcius) + "," + marcaDeTiempo;
+                miArchivo.println(registro);
+                Serial.println(registro);
+                lcd.clear();
+                lcd.setCursor(3, 0);
+                lcd.print("GUARDANDO");
+                lcd.setCursor(3, 1);
+                lcd.print("REGISTRO...");
+                Serial.println("Guardando Registro");
+                delay(1000);
+            }
+            else
+            {
+                Serial.println("¡Lecturas no validas!");
+            }
+            miArchivo.close();
+        }
+        else
+        {
+            Serial.println("¡Error abriendo " + nombreArchivo + "!");
+        }
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if (lecturasSensorHRTSonValidas)
+    {
+        lcd.print("HR:" + String(int(humedadRelativa)) + "     " + String(int(temperaturaCelcius)) + "C");
+    }
+    else
+    {
+        lcd.print("HUMEDAD SUELO");
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("S1:" + String(int(promedioPorcentajesSHS1)) + "%");
+    lcd.setCursor(9, 1);
+    lcd.print("S2:" + String(int(promedioPorcentajesSHS2)) + "%");
+    delay(tiempoEntreLecturasRealizadas);
+    lcd.clear();
+}
